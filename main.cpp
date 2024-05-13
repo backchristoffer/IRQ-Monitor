c3b061fee729e837df9a8886c00ac28b9d6d0758
backchristoffer <backchristoffer@hotmail.com>
2024-05-13 16:37:45 +0200

    revert back to the old code I played with earlier. Now it will parse /proc/interrupts forever in microseconds

--- main.cpp
+++ main.cpp
@@ -1,101 +1,43 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
-#include <vector>
-#include <unordered_map>
-#include <unistd.h>
-#include <stdlib.h>
-#include <getopt.h>
-#include <sched.h>
-#include <cstring>
+#include <sstream>
+#include <chrono>
 
-std::unordered_map<std::string, std::vector<std::string>> parse_interrupts(int cpu) {
-    std::ifstream file("/proc/interrupts");
-    std::unordered_map<std::string, std::vector<std::string>> interrupts;
+using namespace std;
+using namespace chrono;
 
-    if (file.is_open()) {
-        std::string line;
-        bool start_parsing = false;
-        while (std::getline(file, line)) {
-            if (line.find("CPU" + std::to_string(cpu)) != std::string::npos) {
-                start_parsing = true;
-                continue;
-            }
-            if (start_parsing){
-                if (line.find(':') != std::string::npos){
-                    std::istringstream iss(line);
-                    std::string irq;
-                    iss >> irq;
-
-                    std::string devices;
-                    while (iss >> devices) {
-                        interrupts[irq].push_back(devices);
-                }
-            }else {
-                break;
-            }
-            }
-        }
-        file.close();
+void parseInterrupts() {
+    ifstream file("/proc/interrupts");
+    if (!file.is_open()) {
+        cerr << "Error: Could not open /proc/interrupts\n";
+        return;
     }
-    return interrupts;
+
+    string line;
+    getline(file, line);
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string cpu;
+        ss >> cpu;
+        cout << "CPU " << cpu << ":";
+
+        int interruptCount;
+        while (ss >> interruptCount) {
+            microseconds microseconds_duration = duration_cast<microseconds>(milliseconds(interruptCount));
+            cout << " " << microseconds_duration.count();
+        }
+        cout << endl;
+    }
+
+    file.close();
 }
 
-void print_interrupts(const std::unordered_map<std::string, std::vector<std::string>>& interrupts) {
-    std::cout << "IRQ\tDevice" << std::endl;
-    for (const auto& entry : interrupts) {
-        std::cout << entry.first << "\t";
-        for (const auto& device : entry.second) {
-            std::cout << device << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
-static struct option long_options[] = {
-    {"cpu-mon", required_argument, nullptr, 'm'},
-    {nullptr, 0, nullptr, 0}
-};
-
-int main(int argc, char* argv[]) {
-    int cpu_affinity = 0;
-    int cpu_mon = -1; // Default value indicating no specific CPU monitoring
-
-    // Parse command-line arguments
-    int opt;
-    while ((opt = getopt_long(argc, argv, "c:", long_options, nullptr)) != -1) {
-        switch (opt) {
-            case 'c':
-                cpu_affinity = std::atoi(optarg);
-                break;
-            case 'm':
-                cpu_mon = std::atoi(optarg);
-                break;
-            default:
-                std::cerr << "Usage: " << argv[0] << " [-c CPU_AFFINITY] [--cpu-mon CPU_TO_MONITOR]" << std::endl;
-                exit(EXIT_FAILURE);
-        }
-    }
-
-    if (cpu_mon < 0) {
-        // If no specific CPU is provided for monitoring, set the CPU affinity
-        // to the specified CPU affinity for the program
-        cpu_mon = cpu_affinity;
-    }
-
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(cpu_affinity, &cpuset);
-    if (sched_setaffinity(0, sizeof(cpu_set_t), &cpuset) == -1) {
-        perror("sched_setaffinity");
-        exit(EXIT_FAILURE);
-    }
-
+int main() {
     while (true) {
-        auto interrupts = parse_interrupts(cpu_mon);
-        print_interrupts(interrupts);
-        usleep(100000);
+        parseInterrupts();
     }
+
     return 0;
 }
